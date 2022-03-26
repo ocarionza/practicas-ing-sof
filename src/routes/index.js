79223331cd30c66cf0042f62280829e8900e644b
Brayan Ocampo <brayan.ocampo.c7@gmail.com>
2022-03-26 13:14:10 -0500

    feat: update branch to practice superheros

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const peopleRoutes = require("./person.router")
 const departamentsRoutes = require('./departaments.router')
+const superheroRoutes = require('./superhero.router')
 
 function routerApi(app){
     const routes = express.Router();
     app.use("/api/v1", routes); /* endpoint http://localhost:3000/api/v1 */
     routes.use("/people", peopleRoutes); /* Endpoint http://localhost:3000/api/v1/people */
     routes.use('/departaments', departamentsRoutes); /* Endpoint http://localhost:3000/api/v1/departaments */
+    routes.use('/superheros', superheroRoutes); /* Endpoint http://localhost:3000/api/v1/superheros */
 }
 
 module.exports = routerApi;
